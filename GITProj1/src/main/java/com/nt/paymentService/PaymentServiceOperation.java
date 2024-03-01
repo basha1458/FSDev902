946dbe07c16fc8ce33981a302080b4c935a53f73
TL <farooq.shaik.atp3@gmail.com>
2024-03-01 23:29:52 +0530

    Dev1-Brn1-task

--- GITProj1/src/main/java/com/nt/paymentService/PaymentServiceOperation.java
+++ GITProj1/src/main/java/com/nt/paymentService/PaymentServiceOperation.java
@@ -2,4 +2,11 @@ package com.nt.paymentService;
 
 public class PaymentServiceOperation {
 
+	public String doDebitCardPayment() {
+		return "Payment done successfully! through Debit card ";
+	}
+
+	public String doCreditCardPayment() {
+		return "Payment done successfully! through Credit card ";
+	}
 }
