b59eb3bf0498a32152a9c1b3cbbaa1d521720d13
TL <farooq.shaik.atp3@gmail.com>
2024-03-03 17:20:34 +0530

    DEv01-yearlyReport

--- GITProj1/src/main/java/com/nt/report/SalesReport.java
+++ GITProj1/src/main/java/com/nt/report/SalesReport.java
@@ -1,11 +1,15 @@
 package com.nt.report;
 
 public class SalesReport {
-public String MedicalReport() {
+public String weeklyReport() {
 	return "Blood group is A-";
 }
-public String TransportReport() {
+public String MonthReport() {
 	return "Items are arrived safely to factory";
 }
+public String yearlyReport() {
+	return "Items are arrived safely to factory";
+}
+
 }
 //states is way to temporerly store changes that are not ready to commited yet.reapplay when we nedd it.
